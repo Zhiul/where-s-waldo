24ba9f22192cbc93b8749c09bfa130ac72420d9b
Zhiul <scml3023@gmail.com>
2023-05-09 16:29:14 -0400

    Fix vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,6 @@ import fs from "fs";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    https: {
-      key: fs.readFileSync("./.cert/key.pem"),
-      cert: fs.readFileSync("./.cert/cert.pem"),
-    },
     host: true,
   },
   plugins: [svgr(), react()],
