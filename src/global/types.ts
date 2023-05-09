7061899b7038b5545e30d4efcc96a9295e6a67a0
Zhiul <scml3023@gmail.com>
2023-05-09 16:26:48 -0400

    Fix type.ts interfaces declaration

--- src/global/types.ts
+++ src/global/types.ts
@@ -1,47 +1,49 @@
 export {};
 
-interface Image {
-  title: string;
-  high_res_url: string;
-  low_res_url: string;
-  dimensions: {
-    width: number;
-    height: number;
-  };
-  id: string;
-  leaderboard: LeaderboardEntry[];
-  charactersLocation: { [key in ValidCharacter]: RectCoordinates };
-}
+declare global {
+  interface Image {
+    title: string;
+    high_res_url: string;
+    low_res_url: string;
+    dimensions: {
+      width: number;
+      height: number;
+    };
+    id: string;
+    leaderboard: LeaderboardEntry[];
+    charactersLocation: { [key in ValidCharacter]: RectCoordinates };
+  }
 
-interface RectCoordinates {
-  x1: number;
-  x2: number;
-  y1: number;
-  y2: number;
-}
+  interface RectCoordinates {
+    x1: number;
+    x2: number;
+    y1: number;
+    y2: number;
+  }
 
-interface CoordinatesSelection {
-  ImageRectCoordinates: RectCoordinates;
-  length: number;
-}
+  interface CoordinatesSelection {
+    ImageRectCoordinates: RectCoordinates;
+    length: number;
+  }
 
-interface Character {
-  name: string;
-  image: string;
-  found: boolean;
-  iconColor: string;
-}
+  interface Character {
+    name: string;
+    image: string;
+    found: boolean;
+    iconColor: string;
+  }
 
-enum ValidCharacter {
-  Waldo = "waldo",
-  Odlaw = "odlaw",
-  Wenda = "wenda",
-  Wizard = "wizard",
-  Woof = "woof",
-}
+  enum ValidCharacter {
+    Waldo = "waldo",
+    Odlaw = "odlaw",
+    Wenda = "wenda",
+    Wizard = "wizard",
+    Woof = "woof",
+  }
 
-interface LeaderboardEntry {
-  name: string;
-  time: number;
-  id: string;
+  interface LeaderboardEntry {
+    name: string;
+    time: number;
+    id: string;
+  }
 }
